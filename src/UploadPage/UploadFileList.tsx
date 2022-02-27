19ca6674c32538c5ab7c0ca40562da2edde27e70
waymao <my.waymao@gmail.com>
2022-02-26 23:29:37 -0500

    file list

---
+++ src/UploadPage/UploadFileList.tsx
@@ -0,0 +1,45 @@
+import styles from './upload-filelist.module.css';
+import section_styles from './upload-section.module.css';
+import FolderIcon from '../img/file-upload-icon.svg';
+import { ReactComponent as DeleteIcon } from '../img/delete-x-icon.svg';
+import { Button } from 'react-bootstrap'
+import React from 'react';
+
+function FileItem({ file, deleteFile }: {file: File, deleteFile: (file: File) => void}) {
+    return <div className={styles.oneFile}>
+        <span>{file.name}</span>
+        <button 
+            className={styles.removeStyle + " ms-auto " + styles.icoButton} 
+            onClick={() => deleteFile(file)}
+        >
+            <DeleteIcon/>
+        </button>
+    </div>
+}
+
+interface UploadFileListProps {
+    file_list: Map<string, File>, 
+    handleDeleteFile: (file: File) => void,
+    openFileDialogue: () => void,
+};
+
+export function UploadFileList({ file_list, handleDeleteFile, openFileDialogue }: UploadFileListProps) {
+    return <div className={section_styles.mainUploadBox + " " + styles.uploadFileListPanel}>
+        <h4 className="fs-5 d-flex flex-row align-items-center">
+            <img src={FolderIcon} alt="folder icon" style={{width: "1.5rem"}}/>
+            <span className="mx-2 fw-bold">Preview before uploading</span>
+            <Button 
+                className={styles.secondaryButton + " ms-auto addmore-btn " + styles.uploadButton}
+                variant="light"
+            >
+                Add more
+            </Button>
+        </h4>
+        <div className={styles.uploadFileList}>
+            {[...file_list].map(([filename, file]: [string, File]) => <FileItem key={filename} file={file} deleteFile={handleDeleteFile}/>)}
+        </div>
+        <div className="d-flex flex-row mt-auto">
+            <Button className={styles.uploadButton + " mt-2"}>Proceed to upload</Button>
+        </div>
+    </div>;
+}
--- src/UploadPage/UploadSection.tsx
+++ src/UploadPage/UploadSection.tsx
@@ -1,10 +1,12 @@
 import { FileUploader } from "react-drag-drop-files";
+import { useDropzone } from 'react-dropzone';
+import { UploadFileList } from './UploadFileList';
 import styles from './upload-section.module.css';
 import FileUploadIcon from '../img/file-upload-icon.svg';
 import React from "react";
 
 function UploadPromptNoFile({ hover }: {hover: boolean}) {
-    return <div className={styles.addFile}>
+    return <div className={styles.addFile + " " + styles.mainUploadBox}>
         <div className={styles.folderLogo + " mb-4"}>
             <img src={FileUploadIcon}/>
         </div>
@@ -17,23 +19,59 @@ function UploadPromptNoFile({ hover }: {hover: boolean}) {
     </div>;
 }
 
-function UploadPromptHover() {
-    return <div className={styles.addFile}>
-        <div className={styles.folderLogo + " mb-4"}>
-            <img src={FileUploadIcon}/>
-        </div>
-        <p className="fw-bold fs-5">Drop Files here</p>
-    </div>;
-}
-
 
 export default function UploadSection() {
     const [ hover, setHover ] = React.useState(false);
-    const [ file_list, setFileList ] = React.useState<undefined | File[]>();
-    return <div className={styles.uploadToolWrapper + " container"}>
-        <FileUploader multiple onDraggingStateChange={setHover}>
-            <div className={styles.uploadSection}>
-                <UploadPromptNoFile hover={hover}/>
+    const [ file_list, setFileList ] = React.useState<Map<string, File>>(new Map());
+    const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+
+    // used for activating label. not the best practice.
+    const wrapperRef = React.useRef<HTMLDivElement>(null);
+    const openFileDialogue = React.useCallback(() => {
+        const labelRef = wrapperRef.current?.getElementsByTagName('label')?.[0];
+        console.log(labelRef);
+        if (!!labelRef) {
+            labelRef.click();
+        }
+    }, [wrapperRef]);
+
+    const handleAddFile = (files: File[]) => {
+        const new_file_list = new Map(file_list);
+        for (const file of files) {
+            new_file_list.set(file.name + file.size.toString(), file);
+        }
+        setFileList(new_file_list);
+    };
+    const handleDeleteFile = (file: File) => {
+        const new_file_list = new Map(file_list);
+        new_file_list.delete(file.name + file.size.toString());
+        setFileList(new_file_list);
+    }
+
+    const handleOnClick = (e: React.MouseEvent<HTMLElement>) => {
+        console.log(e.target);
+        console.log(e.currentTarget);
+        console.log((e.target as any).className.indexOf("addmore-btn"));
+        const is_file_input = e.target instanceof HTMLInputElement && e.target.type === "file"
+        const is_custom_button = e.target instanceof HTMLElement && 
+                                e.target.className.indexOf("addmore-btn") !== -1;
+        if (is_file_input) {
+            return;
+        } else if (is_custom_button) {
+            e.stopPropagation();
+        } else if (file_list.size !== 0) 
+        {
+            console.log("prevented")
+            e.preventDefault();
+        }
+    }
+
+    return <div className={styles.uploadToolWrapper + " container"} ref={wrapperRef} onClick={handleOnClick}>
+        <FileUploader multiple onDraggingStateChange={setHover} handleChange={handleAddFile}>
+            <div className={styles.uploadSectionBorder}>
+                {file_list.size === 0 ?
+                    <UploadPromptNoFile hover={hover}/> :
+                    <UploadFileList file_list={file_list} handleDeleteFile={handleDeleteFile} openFileDialogue={openFileDialogue}/>}
             </div>
         </FileUploader>
     </div>;
